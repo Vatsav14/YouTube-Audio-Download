a3fc930adc3b04a13dd4764ba548ecabd0d35399
Vatsav Sethupathi <vatsav14@outlook.com>
2020-09-13 06:47:55 -0700

    Update download.py

--- download.py
+++ download.py
@@ -17,9 +17,15 @@ url = sys.argv[1]
 # To get the name with which you want to store the file
 name = sys.argv[2]
 
+# Now download the YouTube video as an mp4 file
 ytd = YouTube(url).streams.first().download(filename="test")
+
+# If you want the video and not the mp3 format, remove everything below this line
 mp4 = "test.mp4"
-mp3 = "C:\\Users\\Vatsav\\Music\\" + name + ".mp3"
+
+# In the next line, change where you want to store the file
+# If you want to change the name of the file, change the value of the name variable
+mp3 = "Where you want to store the file" + name + ".mp3"
 videoclip = VideoFileClip(mp4)
 audioclip = videoclip.audio
 audioclip.write_audiofile(mp3)
